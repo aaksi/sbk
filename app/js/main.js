7262de7892f96ab540ea3fc54c8750f6843bd600
aaksi <yyff91@mail.ru>
2021-02-13 10:28:31 +0300

    added header, top

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,8 @@
 $(function(){
-  
+    
+    $('.burger__btn').on('click',function(){
+        $(this).toggleClass('burger__btn--active'),
+        $('.menu').toggleClass('menu--active')
+    });
 
 });
