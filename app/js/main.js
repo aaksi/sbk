8ea3daaea6f6d2dfc7fe28b60796eae1f105db00
aaksi <yyff91@mail.ru>
2021-02-13 16:13:34 +0300

    added choice, catalog

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,42 @@ $(function(){
         $('.menu').toggleClass('menu--active')
     });
 
+    $('.catalog__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:'<button type="button" class="slider-btn slider-btn--left"></button>',
+        nextArrow:'<button type="button" class="slider-btn slider-btn--right"></button>',
+      });
+
+    $('.gallery__small img').on('click', function(e){
+        if($('.gallery__big img').attr('src')!== $(this).attr('src')){
+
+            $('.gallery__big img').hide().attr('src', $(this).attr('src')).fadeIn(500);
+        }
+
+
+        e.preventDefault();
+    });
+
+    $('.catalog__card').hide();
+
+    $('.furniture__item').on('click', function(){
+        $('.catalog__card--furniture').slideToggle();
+    });
+    // $('.furniture__item2').on('click', function(){
+    //     $('.catalog__card--furniture2').slideToggle();
+    // });
+    $('.table__item').on('click', function(){
+        $('.catalog__card--table').slideToggle();
+    });
+    $('.mattresses__item').on('click', function(){
+        $('.catalog__card--mattresses').slideToggle();
+    });
+    
+    $('.catalog__card-btn--close').on('click', function(){
+        $('.catalog__card').hide(500);
+    });
+    
+    
 });
